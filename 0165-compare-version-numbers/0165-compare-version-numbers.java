f97861c6c15250dd0fbfa893d99ec663ec028bd5
Neelotpal Biswas <neelotpal10@gmail.com>
2023-06-19 01:40:32 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (96.15%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int compareVersion(String version1, String version2) {
-        String[] s1=version1.split("\\.");
-        String[] s2=version2.split("\\.");
-        
-        int maxLen=Math.max(s1.length,s2.length);
-        for(int i=0;i<maxLen;i++){
-            int num1= i<s1.length?Integer.parseInt(s1[i]):0;
-            int num2= i<s2.length?Integer.parseInt(s2[i]):0;
-            if(num1>num2) return 1;
-            else if(num1<num2) return -1;
+    public int compareVersion(String v1, String v2) {
+        int i=0,j=0,len1=v1.length(),len2=v2.length();
+        while(i<len1 || j<len2){
+            int n1=0,n2=0;
+            while(i<len1 && v1.charAt(i)!='.') n1=n1*10+(v1.charAt(i++)-'0');
+            while(j<len2 && v2.charAt(j)!='.') n2=n2*10+(v2.charAt(j++)-'0');
+            if(n1<n2) return -1;
+            else if(n1>n2) return 1;
+            i++;
+            j++;
         }
         return 0;
     }
