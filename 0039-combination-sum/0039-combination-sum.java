d8cbb31f51615fb5729f29bae574020599a66267
Neelotpal Biswas <neelotpal10@gmail.com>
2023-06-19 13:13:35 +0530

    Time: 1 ms (100.00%), Space: 44.1 MB (28.32%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        fun(0,target,arr,ans,new ArrayList<>());
+        return ans;
+    }
+    public void fun(int i,int target,int[] arr,List<List<Integer>> ans,List<Integer> list){
+        if(i==arr.length){
+            if(target==0) ans.add(new ArrayList<>(list));
+            return;
+        }
+        if(arr[i]<=target){
+            list.add(arr[i]);
+            fun(i,target-arr[i],arr,ans,list);
+            list.remove(list.size()-1);
+        }
+        fun(i+1,target,arr,ans,list);
+    }
+}
