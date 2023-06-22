9d2e13c38cd7d220b5d629c65a1969fd035961f6
Neelotpal Biswas <neelotpal10@gmail.com>
2023-06-22 12:25:23 +0530

    Time: 1 ms (95.26%), Space: 42 MB (94.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,15 +1,21 @@
 class Solution {
-    public void solve(List<String> ans,int n,int open,int close,String sb){
+    public void solve(List<String> ans,int n,int open,int close,StringBuilder sb){
         if(sb.length()==n*2){
-            ans.add(sb);
+            ans.add(sb.toString());
             return;
         }
-        if(open<n) solve(ans,n,open+1,close,sb+"(");
-        if(close<open) solve(ans,n,open,close+1,sb+")");
+        if(open<n){
+            solve(ans,n,open+1,close,sb.append('('));
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close<open){
+            solve(ans,n,open,close+1,sb.append(')'));
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
     public List<String> generateParenthesis(int n) {
         List<String> ans=new ArrayList<>();
-        solve(ans,n,0,0,"");
+        solve(ans,n,0,0,new StringBuilder());
         return ans;
     }
 }
