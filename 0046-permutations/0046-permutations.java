83ac4d959783f1d2adb3074ebe68c864cb07361d
Neelotpal Biswas <neelotpal10@gmail.com>
2023-06-23 12:53:51 +0530

    Time: 1 ms (98.89%), Space: 44.1 MB (18.84%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,25 @@
 class Solution {
-    public void solve(List<List<Integer>> ans,List<Integer> list,int[] nums,int[] visited){
-        if(list.size()==nums.length){
-            ans.add(new ArrayList<>(list));
+    public void swap(int i,int j,int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void solve(int idx,List<List<Integer>> ans,int[] nums){
+        if(idx==nums.length){
+            List<Integer> list=new ArrayList<>();
+            for(int i:nums) list.add(i);
+            ans.add(list);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(visited[i]==0){
-                visited[i]=1;
-                list.add(nums[i]);
-                solve(ans,list,nums,visited);
-                list.remove(list.size()-1);
-                visited[i]=0;
-            }
+        for(int i=idx;i<nums.length;i++){
+            swap(idx,i,nums);
+            solve(idx+1,ans,nums);
+            swap(idx,i,nums);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        int[] visited=new int[nums.length];
-        solve(ans,new ArrayList<>(),nums,visited);
+        solve(0,ans,nums);
         return ans;
     }
 }
