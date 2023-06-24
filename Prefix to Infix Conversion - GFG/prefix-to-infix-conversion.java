0d4dd7514440c705ef1695e96d9e2ddc611fe565
Neelotpal Biswas <neelotpal10@gmail.com>
2023-06-24 18:28:23 +0530

    Added solution - LeetHub

---
+++ Prefix to Infix Conversion - GFG/prefix-to-infix-conversion.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            String ans = obj.preToInfix(s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static String preToInfix(String pre_exp) {
+        // code here
+        Stack<String> st=new Stack<>();
+        for(int i=pre_exp.length()-1;i>=0;i--){
+            char c=pre_exp.charAt(i);
+            if(c>='a' && c<='z' || c>='A' && c<='Z') st.push(Character.toString(c));
+            else st.push("("+st.pop()+c+st.pop()+")");
+        }
+        return st.pop();
+    }
+}
