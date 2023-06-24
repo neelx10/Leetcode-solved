12fa562bbd3e9b00ff91d7c8ceda1208ef8b30ed
Neelotpal Biswas <neelotpal10@gmail.com>
2023-06-25 02:18:17 +0530

    Time: 34 ms (67.21%), Space: 43.8 MB (75.00%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.java
+++ 0956-tallest-billboard/0956-tallest-billboard.java
@@ -1,18 +1,21 @@
 class Solution {
-    int[][] dp=new int[21][10003];
-    public int solve(int[] rods,int idx, int diff){
-        if(idx==rods.length){
-            if(diff==0) return 0;
-            return (int)-1e9;
+    public int solve(int[] rods, int idx, int diff, int[][] dp, int sum) {
+        if (idx == rods.length) {
+            if (diff == 0) return 0;
+            return (int) -1e9;
         }
-        if(dp[idx][diff+5000]!=-1) return dp[idx][diff+5000];
-        int nothing=solve(rods,idx+1,diff);
-        int l1rod=rods[idx]+solve(rods,idx+1,diff+rods[idx]);
-        int l2rod=rods[idx]+solve(rods,idx+1,diff-rods[idx]);
-        return dp[idx][diff+5000]=Math.max(nothing,Math.max(l1rod,l2rod));
+        if (dp[idx][diff+sum] != -1) return dp[idx][diff+sum];
+        int nothing = solve(rods, idx + 1, diff, dp, sum);
+        int l1rod = rods[idx] + solve(rods, idx + 1, diff + rods[idx], dp, sum);
+        int l2rod = rods[idx]+solve(rods, idx + 1, diff - rods[idx], dp, sum);
+        return dp[idx][diff+sum] = Math.max(nothing, Math.max(l1rod, l2rod));
     }
+    
     public int tallestBillboard(int[] rods) {
-        for(int[] r:dp) Arrays.fill(r,-1);
-        return solve(rods,0,0)/2;
+        int sum = 0;
+        for (int i : rods) sum += i;
+        int[][] dp = new int[rods.length + 1][2 * sum + 1];  // Adjust the size of dp array
+        for (int[] r : dp) Arrays.fill(r, -1);
+        return solve(rods, 0, 0, dp, sum) / 2;
     }
 }
