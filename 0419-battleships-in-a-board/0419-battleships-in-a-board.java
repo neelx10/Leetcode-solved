6c82e8db254f48b8b6e2dcc6e4e2a0ba863f0a89
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-01 14:34:07 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (23.61%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void dfs(char[][] grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!='X') return ;
+        
+        grid[i][j]='#';
+		
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        
+    }
+    public int countBattleships(char[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='X'){  
+                    dfs(grid,i,j);
+                    count++;
+                }
+            } 
+        }
+        return count;
+    }
+}
+
+
+
+
+
+
+
+
