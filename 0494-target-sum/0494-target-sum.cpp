d7bab52ce7f480413d6c3fb8ce246028ece6dcb2
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-01 11:18:56 +0530

    Time: 28 ms (51.85%), Space: 22.4 MB (22.93%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int f(int ind, int target, vector<int>& nums, unordered_map<string, int>& memo) {
-        string key = to_string(ind) + "," + to_string(target);
+    int total;
+    
+    int findTargetSumWays(vector<int>& nums, int S) {
+        total = accumulate(nums.begin(), nums.end(), 0);
         
-        if (ind < 0) {
-            if (target == 0) return 1;
+        vector<vector<int>> memo(nums.size(), vector<int>(2 * total + 1, INT_MIN));
+        
+        return calculate(nums, 0, 0, S, memo);
+    }
+    
+    int calculate(vector<int>& nums, int i, int sum, int S, vector<vector<int>>& memo) {
+        if (i == nums.size()) {
+            if (sum == S) return 1;
             else return 0;
         }
         
-        if (memo.count(key)) return memo[key];
+        if (memo[i][sum + total] != INT_MIN) return memo[i][sum + total];
         
-        int pos = f(ind - 1, target - nums[ind], nums, memo);
-        int neg = f(ind - 1, target + nums[ind], nums, memo);
-        
-        memo[key] = pos + neg;
-        return memo[key];
-    }
-    
-    int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> memo;
-        return f(nums.size() - 1, target, nums, memo);
+        int add = calculate(nums, i + 1, sum + nums[i], S, memo);
+        int subtract = calculate(nums, i + 1, sum - nums[i], S, memo);
+        return memo[i][sum + total] = add + subtract;   
     }
 };
