8241fa5bf567f061dfc4674fdff4c7181316a7aa
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-02 17:38:56 +0530

    Added solution - LeetHub

---
+++ Alternate positive and negative numbers - GFG/alternate-positive-and-negative-numbers.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().rearrange(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    void rearrange(int arr[], int n) {
+        // code here
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        for (int i:arr){
+            if (i >= 0) pos.add(i);
+            else neg.add(i);
+        }
+        
+        int i = 0, j = 0, k = 0;
+        while (i < pos.size() && j < neg.size()) {
+            arr[k++] = pos.get(i++);
+            arr[k++] = neg.get(j++);
+        }
+        while (i < pos.size()) arr[k++] = pos.get(i++);
+        while (j < neg.size()) arr[k++] = neg.get(j++);
+    }
+}
