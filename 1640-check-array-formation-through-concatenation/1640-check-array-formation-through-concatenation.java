1bb83d63f1036f96bb89109d57a62c29aa6933c8
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-04 04:29:46 +0530

    Time: 1 ms (87.37%), Space: 41.2 MB (52.63%) - LeetHub

---
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) map.put(arr[i], i);
+        for (int[] piece : pieces) {
+            if (!map.containsKey(piece[0])) return false;
+            int index = map.get(piece[0]);
+            for (int i = 1; i < piece.length; i++) {
+                if (!map.containsKey(piece[i]) || map.get(piece[i]) != index + i) return false;
+            }
+        }
+        return true;
+    }
+}
