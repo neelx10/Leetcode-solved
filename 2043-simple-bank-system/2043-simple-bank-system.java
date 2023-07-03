4f7622d30cd998426bc0263bc8d64d06781f60f4
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-04 04:19:55 +0530

    Time: 141 ms (25.33%), Space: 102.6 MB (30.57%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.java
+++ 2043-simple-bank-system/2043-simple-bank-system.java
@@ -1,15 +1,15 @@
 class Bank {
     int n;
-    long[] arr;
+    private long[] balance;
     public Bank(long[] balance) {
         n=balance.length;
-        arr=balance;
+        this.balance=balance;
     }
     
     public boolean transfer(int account1, int account2, long money) {
-        if(account1>0 && account1<=n && account2>0 && account2<=n && arr[account1-1]>=money){
-            arr[account1-1]-=money;
-            arr[account2-1]+=money;
+        if(account1>0 && account1<=n && account2>0 && account2<=n && balance[account1-1]>=money){
+            balance[account1-1]-=money;
+            balance[account2-1]+=money;
             return true;
         }
         return false;
@@ -17,15 +17,15 @@ class Bank {
     
     public boolean deposit(int account, long money) {
         if(account>0 && account<=n){
-            arr[account-1]+=money;
+            balance[account-1]+=money;
             return true;
         }
         return false;
     }
     
     public boolean withdraw(int account, long money) {
-        if(account>0 && account<=n && arr[account-1]>=money){
-            arr[account-1]-=money;
+        if(account>0 && account<=n && balance[account-1]>=money){
+            balance[account-1]-=money;
             return true;
         }
         return false;
