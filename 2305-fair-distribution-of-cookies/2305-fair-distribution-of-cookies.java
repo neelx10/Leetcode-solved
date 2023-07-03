9e13c4b4d593e48a54d933e9ddd40eb668f3d50d
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-03 11:56:48 +0530

    Time: 402 ms (46.53%), Space: 40.4 MB (10.24%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,41 +1,22 @@
 class Solution {
-    private int dfs(int i, int[] distribute, int[] cookies, int k, int zeroCount) {
-        // If there are not enough cookies remaining, return Integer.MAX_VALUE
-        // as it leads to an invalid distribution.
-        if (cookies.length - i < zeroCount) {
-            return Integer.MAX_VALUE;   
-        }
-
-        // After distributing all cookies, return the unfairness of this
-        // distribution.
-        if (i == cookies.length) {
-            int unfairness = Integer.MIN_VALUE;
-            for (int value : distribute) {
-                unfairness = Math.max(unfairness, value);
-            }
-            return unfairness;
+    int ans=(int)1e9;
+    public void dfs(int[] cookies,int[] arr,int k,int i){
+        if(i==cookies.length){
+            int uf=(int)-1e9;
+            for(int num:arr) uf=Math.max(uf,num);
+            ans=Math.min(ans,uf);
+            return;
         }
         
-        // Try to distribute the i-th cookie to each child, and update answer
-        // as the minimum unfairness in these distributions.
-        int answer = Integer.MAX_VALUE;
-        for (int j = 0; j < k; ++j) {
-            zeroCount -= distribute[j] == 0 ? 1 : 0;
-            distribute[j] += cookies[i];
-            
-            // Recursively distribute the next cookie.
-            answer = Math.min(answer, dfs(i + 1, distribute, cookies, k, zeroCount));
-            
-            distribute[j] -= cookies[i];
-            zeroCount += distribute[j] == 0 ? 1 : 0;
+        for(int j=0;j<k;j++){
+            arr[j]+=cookies[i];
+            dfs(cookies,arr,k,i+1);
+            arr[j]-=cookies[i];
         }
-        
-        return answer;
     }
-    
     public int distributeCookies(int[] cookies, int k) {
-        int[] distribute = new int[k];
-        
-        return dfs(0, distribute, cookies, k, k);
+        int[] arr=new int[k];
+        dfs(cookies,arr,k,0);
+        return ans;
     }
 }
