96692766ea1ee3b8f49a663053e9691cf37782a6
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-07 11:58:01 +0530

    Time: 14 ms (84.69%), Space: 43.6 MB (65.00%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,33 +1,16 @@
 class Solution {
-    private Boolean canMakeValidSubstring(String s,int substringLength,int k){
-        // take a window of length `substringLength` on the given
-        // string, and move it from left to right. If this window
-        // satisfies the condition of a valid string, then we return
-        // true
-        int[] freqMap = new int[26];
-        int maxFrequency = 0;
-        int start = 0;
-        for (int end = 0; end < s.length(); end += 1) {
-            freqMap[s.charAt(end) - 'A'] ++;
-            if (end + 1 - start > substringLength) freqMap[s.charAt(start++) - 'A'] --;
-            maxFrequency = Math.max(maxFrequency, freqMap[s.charAt(end) - 'A']);
-            if (substringLength - maxFrequency <= k) return true;
-        }
-        return false;
-    }
-    
     public int maxConsecutiveAnswers(String s, int k) {
-        // binary search over the length of substring
-        // lo contains the valid value, and hi contains the
-        // invalid value
-        int lo = 1;
-        int hi = s.length() + 1;
-        while (lo + 1 < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (canMakeValidSubstring(s, mid, k)) lo = mid;
-            else hi = mid;
+        int start = 0;
+        int[] frequencyMap = new int[26];
+        int maxFrequency = 0;
+        int longestSubstringLength = 0;
+
+        for (int end = 0; end < s.length(); end ++) {
+            frequencyMap[s.charAt(end) - 'A']++;
+            maxFrequency = Math.max(maxFrequency, frequencyMap[s.charAt(end) - 'A']);
+            if (!(end + 1 - start - maxFrequency <= k)) frequencyMap[s.charAt(start++) - 'A']--;
+            longestSubstringLength = end + 1 - start;
         }
-        return lo;
+        return longestSubstringLength;
     }
 }
-
