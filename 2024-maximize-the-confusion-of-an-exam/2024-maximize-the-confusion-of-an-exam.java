bc9d686b5dd6ffb023c92b37f61af3646d5c9073
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-07 11:52:14 +0530

    Time: 87 ms (5.63%), Space: 43.5 MB (75.94%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,32 +1,33 @@
 class Solution {
-    private Boolean isWindowValid(int start, int end, int count, int k) {
-        return end + 1 - start - count <= k;
+    private Boolean canMakeValidSubstring(String s,int substringLength,int k){
+        // take a window of length `substringLength` on the given
+        // string, and move it from left to right. If this window
+        // satisfies the condition of a valid string, then we return
+        // true
+        int[] freqMap = new int[26];
+        int maxFrequency = 0;
+        int start = 0;
+        for (int end = 0; end < s.length(); end += 1) {
+            freqMap[s.charAt(end) - 'A'] ++;
+            if (end + 1 - start > substringLength) freqMap[s.charAt(start++) - 'A'] --;
+            maxFrequency = Math.max(maxFrequency, freqMap[s.charAt(end) - 'A']);
+            if (substringLength - maxFrequency <= k) return true;
+        }
+        return false;
     }
     
     public int maxConsecutiveAnswers(String s, int k) {
-        HashSet<Character> allLetters = new HashSet();
-
-        // collect all unique letters
-        for (int i = 0; i < s.length(); i++) allLetters.add(s.charAt(i));
-
-        int maxLength = 0;
-        for (Character letter : allLetters) {
-            int start = 0;
-            int count = 0;
-            // initialize a sliding window for each unique letter
-            // if the letter matches, increase the count
-            for (int end = 0; end < s.length(); end += 1) {
-                if (s.charAt(end) == letter) count ++;
-                // bring start forward until the window is valid again
-                while (!isWindowValid(start, end, count, k)) {
-                    // if the letter matches, decrease the count
-                    if (s.charAt(start) == letter) count --;
-                    start ++;
-                }
-                // at this point the window is valid, update maxLength
-                maxLength = Math.max(maxLength, end + 1 - start);
-            }
+        // binary search over the length of substring
+        // lo contains the valid value, and hi contains the
+        // invalid value
+        int lo = 1;
+        int hi = s.length() + 1;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (canMakeValidSubstring(s, mid, k)) lo = mid;
+            else hi = mid;
         }
-        return maxLength;
+        return lo;
     }
 }
+
