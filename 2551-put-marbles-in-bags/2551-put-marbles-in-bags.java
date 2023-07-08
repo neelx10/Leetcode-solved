7fe628461cc75cb4ae651acecc6b8a98fa18a40f
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-08 13:35:25 +0530

    Time: 37 ms (97.73%), Space: 60 MB (12.50%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+        int[] pairWeights = new int[n - 1];
+        for (int i = 0; i < n - 1; ++i) pairWeights[i] = weights[i] + weights[i + 1];
+        Arrays.sort(pairWeights);
+        // Get the difference between the largest k - 1 values and the 
+        // smallest k - 1 values.
+        long answer = 0l;
+        for (int i = 0; i < k - 1; ++i) answer += pairWeights[n - 2 - i] - pairWeights[i];
+        return answer;
+    }
+}
