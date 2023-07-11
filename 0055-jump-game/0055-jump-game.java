d561d47e04ee33d4d7389a938aef024ff43dbf73
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-11 21:56:09 +0530

    Time: 3 ms (37.89%), Space: 44.1 MB (74.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int lastPos = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) lastPos = i; 
+        int reach = 0; 
+        for(int idx = 0; idx < nums.length; idx++) {
+            if(reach==nums.length-1) return true;
+            if(reach < idx) return false;
+            reach = Math.max(reach, idx + nums[idx]);
         }
-        return lastPos == 0;
+        return true;
     }
 }
