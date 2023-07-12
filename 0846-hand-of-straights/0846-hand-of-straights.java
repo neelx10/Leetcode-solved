741834dbffcb4172a58006964bc2dce2065b661f
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-12 16:24:34 +0530

    Time: 49 ms (52.27%), Space: 44.7 MB (33.28%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,18 +1,12 @@
 class Solution {
-    public boolean isNStraightHand(int[] nums, int k) {
-        if(nums.length % k!=0) return false;
-        Arrays.sort(nums);
-        Map<Integer,Integer> map=new LinkedHashMap<>();
-        //hashmap doesnt guarantee the insertion order
-        //use linkedhashmap so that insertion order remains intact
-        for(int i:nums) map.put(i,map.getOrDefault(i,0)+1);
-
-        while(map.size()>0){
-            int temp = map.keySet().iterator().next();
-            for(int i=temp;i<temp+k;i++){
-                if(!map.containsKey(i)) return false;
-                if(map.get(i)==1) map.remove(i);
-                else map.put(i,map.get(i)-1);
+    public boolean isNStraightHand(int[] hand, int W) {
+        if(hand.length % W != 0) return false;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int elem: hand) pq.add(elem);
+        while(!pq.isEmpty()){
+            int top = pq.poll();
+            for(int i=1; i<W; i++){
+                if(!pq.remove(top+i)) return false;
             }
         }
         return true;
