4bf7855cd230b775025ee3b50dda441e1593458c
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-14 12:57:12 +0530

    Time: 36 ms (44.98%), Space: 58.1 MB (45.38%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -1,14 +1,13 @@
 class Solution {
-    public long solve(int ind,int[] nums,int flag,long[][] dp){
-        if(ind==nums.length) return 0;
-        if(dp[ind][flag]!=-1) return dp[ind][flag];
-        if(flag==0) dp[ind][flag]=Math.max(nums[ind]+solve(ind+1,nums,1,dp),solve(ind+1,nums,0,dp));
-        else dp[ind][flag]=Math.max(-nums[ind]+solve(ind+1,nums,0,dp),solve(ind+1,nums,1,dp));
-        return dp[ind][flag];
-    }
     public long maxAlternatingSum(int[] nums) {
-        long[][] dp=new long[nums.length][2];
-        for(long[] r:dp) Arrays.fill(r,-1);
-        return solve(0,nums,0,dp);
+        int n=nums.length;
+        long[][] dp=new long[n+1][2];
+        for(int ind=n-1;ind>=0;ind--){
+            for(int flag=1;flag>=0;flag--){
+                if(flag==0) dp[ind][flag]=Math.max(nums[ind]+dp[ind+1][1],dp[ind+1][0]);
+                else dp[ind][flag]=Math.max(-nums[ind]+dp[ind+1][0],dp[ind+1][1]);
+            }
+        }
+        return dp[0][0];
     }
 }
