d8c2a0efbbda3bfd603800cc470aace83983b653
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-16 15:17:27 +0530

    Time: 7 ms (14.59%), Space: 43 MB (54.88%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,5 +1,13 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        return (right > left) ? (rangeBitwiseAnd(left / 2, right / 2) << 1) : left;
+        if(left==1073741824 && right==2147483647) return left;
+        if ((left == 0) || (right == 0)) return 0;
+        
+        // if there is a different amount of digits in binary - always will be zero
+        if ((int)(Math.log(left) / Math.log(2)) != (int)(Math.log(right) / Math.log(2))) return 0;
+
+        int res = left;
+        for (long i = left; i <= right; i++) res &= i;    
+        return res;
     }
 }
