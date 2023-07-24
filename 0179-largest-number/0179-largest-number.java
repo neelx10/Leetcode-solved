0c82b24d83973c12232e0e272e6dd1155689ad7b
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-24 13:27:25 +0530

    Time: 5 ms (97.28%), Space: 43.2 MB (55.11%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,29 +1,18 @@
-class CustomComparator implements Comparator<String> {
-    @Override
-    public int compare(String num1, String num2) {
-        return (num1 + num2).compareTo(num2 + num1);
-    }
-}
-    
-
 class Solution {
     public String largestNumber(int[] nums) {
-        String[] strNums = new String[nums.length];
+        int n=nums.length;
+        String[] strNums = new String[n];
         boolean zeros=true;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < n; i++) {
             strNums[i] = String.valueOf(nums[i]);
             if(nums[i]!=0) zeros=false;
         }
-        
         if(zeros) return "0";
-
-        // Sorting the array based on the custom comparator
-        Arrays.sort(strNums, new CustomComparator());
+        
+        Arrays.sort(strNums, (a,b)->(a + b).compareTo(b + a));
         
         StringBuilder sb=new StringBuilder();
-        for(int i=nums.length-1;i>=0;i--){
-            sb.append(strNums[i]);
-        }
+        for(int i=n-1;i>=0;i--) sb.append(strNums[i]);
         return sb.toString();
     }
 }
