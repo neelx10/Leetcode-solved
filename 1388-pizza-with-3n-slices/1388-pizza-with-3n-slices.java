3d5e570137249c372eb7f6f47b08cb56181718bc
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-25 15:04:23 +0530

    Time: 6 ms (96.85%), Space: 43.4 MB (55.12%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.java
@@ -1,17 +1,24 @@
 class Solution {
-    public int solve(int start,int end,int[] slices, int n,int[][] dp) {
-        if(n==0 || start>end) return 0;
-        if(dp[start][n]!=-1) return dp[start][n];
-        return dp[start][n]=Math.max(solve(start+1,end,slices,n,dp),slices[start]+solve(start+2,end,slices,n-1,dp));
-    }
-    
     public int maxSizeSlices(int[] slices) {
-        int n=slices.length;
-        int[][] dp1=new int[n][n];
-        for(int[] r:dp1) Arrays.fill(r,-1);
-        int[][] dp2=new int[n][n];
-        for(int[] r:dp2) Arrays.fill(r,-1);
-        return Math.max(solve(0,n-2,slices,n/3,dp1),solve(1,n-1,slices,n/3,dp2));
+        int n = slices.length;
+        int k = n / 3;
+
+        int[][] dp1 = new int[n+1][k + 1];
+        int[][] dp2 = new int[n+2][k + 1];
+        
+        for (int i = n-2; i>=0; i--) {
+            for (int j = 1; j <= k; j++) {
+                dp1[i][j]=Math.max(dp1[i+1][j],slices[i]+dp1[i+2][j-1]);
+            }
+        }
+
+        for (int i = n-1; i>=1; i--) {
+            for (int j = 1; j <= k; j++) {
+                dp2[i][j]=Math.max(dp2[i+1][j],slices[i]+dp2[i+2][j-1]);
+            }
+        }
+        
+        return Math.max(dp1[0][k], dp2[1][k]);
     }
 }
 
