3a0a0d976fb57534f194162060015b8024cea5b7
Neelotpal Biswas <neelotpal10@gmail.com>
2023-07-27 11:28:29 +0530

    Time: 21 ms (64.55%), Space: 57.7 MB (8.18%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long sumPower = 0;
+        for (int power : batteries) sumPower += power;
+        long left = 1, right = sumPower / n;
+        while (left <= right){
+            long target = left + (right-left)/2;
+            long extra = 0;
+            
+            for (int power : batteries) extra += Math.min(power, target);
+
+            if (extra >= (long)(n * target)) left = target+1;
+            else right = target - 1;
+        }
+        return right;
+    }
+}
