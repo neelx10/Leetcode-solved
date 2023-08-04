4c8309b90f0163058a738e5a15c64e42356c7ead
Neelotpal Biswas <neelotpal10@gmail.com>
2023-08-04 12:56:03 +0530

    Time: 2 ms (88.87%), Space: 41.3 MB (69.18%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -11,43 +11,43 @@ class Pair{
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        Queue<Pair> q=new LinkedList<>();
-        int[][] visited=new int[m][n];
-        int freshOranges=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) freshOranges++;
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] visited=new int[n][m];
+        int fresh=0;
+        Queue<int[]> q=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) fresh++;
                 else if(grid[i][j]==2){
                     visited[i][j]=1;
-                    q.add(new Pair(i,j,0));                           //0 --> time
+                    q.offer(new int[]{i,j});
                 }
             }
         }
-
+        
+        int[] dr={-1,0,1,0};
+        int[] dc={0,1,0,-1};
         int count=0;
-        int minTime=0;
-        int[] delr={-1,0,1,0};
-        int[] delc={0,1,0,-1};
+        int time=0;
         while(!q.isEmpty()){
-            int r=q.peek().row;
-            int c=q.peek().col;
-            int t=q.peek().time;
-            minTime=Math.max(minTime,t);
-            q.poll();
-            for(int i=0;i<4;i++){
-                int nrow=r+delr[i];
-                int ncol=c+delc[i];
-                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && visited[nrow][ncol] == 0 && grid[nrow][ncol] == 1){
-                    visited[nrow][ncol]=1;
-                    q.add(new Pair(nrow,ncol,t+1));
-                    count++;
+            int len=q.size();
+            time++;
+            while(len-- > 0){
+                int[] temp=q.poll();
+                int row=temp[0];
+                int col=temp[1];
+                for(int i=0;i<4;i++){
+                    int nrow=row+dr[i];
+                    int ncol=col+dc[i];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                        visited[nrow][ncol]=1;
+                        count++;
+                        q.offer(new int[]{nrow,ncol});
+                    }
                 }
             }
         }
-
-        if(count!=freshOranges) return -1;
-        return minTime;
+        return (count!=fresh)?-1:(time==0)?0:time-1;
     }
 }
