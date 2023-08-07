d426a5b590e9bd0fb7f60e68bb8a7d6cef4a23e1
Neelotpal Biswas <neelotpal10@gmail.com>
2023-08-07 16:56:35 +0530

    Time: 18 ms (30.56%), Space: 44 MB (15.70%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -34,29 +34,24 @@ class DisjointSet {
 class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        int totalElms = n * n;
-        int[][] map = new int[totalElms][2];
-        int[][] vis=new int[n][n];
-        for(int i = 0; i < n; i++) { 
-            for(int j = 0; j < n; j++) {
-                map[grid[i][j]] = new int[]{i,j};
-            }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.add(new int[]{0,0,grid[0][0]});
+        boolean[][] vis = new boolean[n][n];
+        int[][] dir = {{0,1},{1,0},{-1,0},{0,-1}};
+        while(!pq.isEmpty()){
+            int[] temp = pq.poll();
+            int row=temp[0],col=temp[1],time=temp[2];
+            if(row == n - 1 && col == n - 1) return time;
+            vis[row][col] = true;
+            for(int i = 0; i < 4; i++){
+                int nrow = row + dir[i][0];
+                int ncol = col + dir[i][1];
+                if(nrow>=0 && ncol>=0 && nrow < n && ncol < n && vis[nrow][ncol] == false){
+                    pq.offer(new int[]{nrow, ncol, Math.max(time , grid[nrow][ncol])});
+                }
+            } 
         }
-        DisjointSet ds = new DisjointSet(totalElms);
-        int delRow[] = {0, 0, 1, -1};
-        int delCol[] = {1, -1, 0, 0};
-        int lastElm = totalElms - 1;
-        for(int time = 0; time < totalElms;time++) {
-            int x=map[time][0],y=map[time][1];
-            vis[x][y]=1;
-            for (int k = 0; k < 4; k++){
-                int nrow = x + delRow[k];
-                int ncol = y + delCol[k];
-                if (nrow>=0 && nrow<n && ncol>=0 && ncol<n && vis[nrow][ncol] == 1) ds.unionBySize(x*n+y,nrow*n+ncol);
-            }
-            if (ds.findUPar(0) == ds.findUPar(lastElm)) return time;
-        }
-        return lastElm;
+        return -1;
     }
 }
 
