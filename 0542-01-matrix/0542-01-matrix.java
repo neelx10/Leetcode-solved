12a262177b2eebbca34b417a1ff42a51b84bf5ea
Neelotpal Biswas <neelotpal10@gmail.com>
2023-08-17 15:52:38 +0530

    Time: 17 ms (33.15%), Space: 46.4 MB (26.73%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -10,7 +10,7 @@ class Solution {
                 if(mat[i][j]==0){
                     visited[i][j]=1;
                     ans[i][j]=0;
-                    q.offer(new int[]{i,j,0});
+                    q.offer(new int[]{i,j});
                 }
             }
         }
@@ -19,7 +19,6 @@ class Solution {
             int[] temp=q.poll();
             int r=temp[0];
             int c=temp[1];
-            int s=temp[2];
             int[] delrow={-1,0,1,0};
             int[] delcol={0,-1,0,1};
             for(int i=0;i<4;i++){
@@ -27,8 +26,8 @@ class Solution {
                 int ncol=c+delcol[i];
                 if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && mat[nrow][ncol]==1 && visited[nrow][ncol]==0){
                     visited[nrow][ncol]=1;
-                    ans[nrow][ncol]=s+1;
-                    q.offer(new int[]{nrow,ncol,s+1});
+                    ans[nrow][ncol]=ans[r][c]+1;
+                    q.offer(new int[]{nrow,ncol});
                 }
             }
         }
