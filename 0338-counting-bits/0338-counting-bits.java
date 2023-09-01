c8d9526e932b99d8684e191e6b0c90ef21bde2a7
Neelotpal Biswas <neelotpal10@gmail.com>
2023-09-01 10:55:51 +0530

    Time: 3 ms (35.70%), Space: 46.7 MB (25.57%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans=new int[n+1];
+        int[] BitsSetTable256 = new int[256];
+        for (int i = 0; i < 256; i++) BitsSetTable256[i] = (i & 1) + BitsSetTable256[i / 2];
         for(int i=0;i<=n;i++){
             int temp=i;
-            while (temp > 0) {
-                ans[i] += temp & 1;
-                temp >>= 1;
-            }
+            ans[i] = (BitsSetTable256[temp & 0xff] + BitsSetTable256[(temp >> 8) & 0xff] + BitsSetTable256[(temp >> 16) & 0xff]+ BitsSetTable256[temp >> 24]);
         }
         return ans;
     }
