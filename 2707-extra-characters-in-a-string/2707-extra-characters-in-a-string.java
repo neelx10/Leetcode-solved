80d7f8d17b147000cf90e391982b6b6246669918
Neelotpal Biswas <neelotpal10@gmail.com>
2023-09-02 14:39:41 +0530

    Time: 47 ms (47.71%), Space: 44.2 MB (56.00%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,24 +1,20 @@
 class Solution {
-    public int solve(int i,String s,Set<String> set, int n,int[] dp){
-        if(i==n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        StringBuilder sb=new StringBuilder();
-        int mini=n;
-        for(int ind=i;ind<n;ind++){
-            sb.append(s.charAt(ind));
-            int temp1=(set.contains(sb.toString()))?0:sb.length();
-            int temp2=solve(ind+1,s,set,n,dp);
-            mini=Math.min(mini,temp1+temp2);
-        }
-        return dp[i]=mini;
-    }
-    
     public int minExtraChar(String s, String[] dictionary) {
         int n=s.length();
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
+        int[] dp=new int[n+1];
         Set<String> set=new HashSet<>();
         for(String st:dictionary) set.add(st);
-        return solve(0,s,set,n,dp);
+        for(int i=n-1;i>=0;i--){
+            StringBuilder sb=new StringBuilder();
+            int mini=n;
+            for(int ind=i;ind<n;ind++){
+                sb.append(s.charAt(ind));
+                int temp1=(set.contains(sb.toString()))?0:sb.length();
+                int temp2=dp[ind+1];
+                mini=Math.min(mini,temp1+temp2);
+            }
+            dp[i]=mini;
+        }
+        return dp[0];
     }
 }
