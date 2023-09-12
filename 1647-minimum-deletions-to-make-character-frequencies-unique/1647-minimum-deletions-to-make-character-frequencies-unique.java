773a753dd913c9840acf245897dd288c2b5f831a
Neelotpal Biswas <neelotpal10@gmail.com>
2023-09-12 14:25:57 +0530

    Time: 7 ms (98.85%), Space: 44 MB (96.87%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,22 +1,15 @@
 class Solution {
     public int minDeletions(String s) {
-        int[] freq=new int[26];
-        Set<Integer> set=new HashSet<>();
-        for(char c:s.toCharArray()) freq[c-'a']++;
-        Arrays.sort(freq);
-        int count=0;
-        for(int i=25;i>=0;i--){
-            if(freq[i]!=0){
-                if(!set.contains(freq[i])) set.add(freq[i]);
-                else{
-                    while(set.contains(freq[i]) && freq[i]!=0){
-                        count++;
-                        freq[i]--;
-                    }
-                    if(freq[i]!=0) set.add(freq[i]);
-                }
+        int[] freq = new int[26];
+        Set<Integer> set = new HashSet<>();
+        for (char c : s.toCharArray()) freq[c - 'a']++;
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            while (freq[i] != 0 && set.contains(freq[i])) {
+                freq[i]--;
+                count++;
             }
-            else break;
+            if (freq[i] != 0) set.add(freq[i]);
         }
         return count;
     }
