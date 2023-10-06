4849a6b3bc16484362980aae4a461a3d55bb026f
Neelotpal Biswas <neelotpal10@gmail.com>
2023-10-06 19:50:10 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (51.27%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] memo;
+    public int dp(int num) {
+        if (num <= 3) return num;
+        if (memo[num] != 0) return memo[num];
+        int ans = num;
+        for (int i = 2; i < num; i++) ans = Math.max(ans, i * dp(num - i));
+        return memo[num] = ans;
+    }
+    
+    public int integerBreak(int n) {
+        if (n <= 3) return n - 1;
+        memo = new int[n + 1];
+        return dp(n);
+    }
+}
