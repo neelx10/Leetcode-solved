580d800e8c8dca7518d6b703dd14b26530ee7559
Neelotpal Biswas <neelotpal10@gmail.com>
2023-10-17 18:52:21 +0530

    Added solution - LeetHub

--- Transitive closure of a Graph - GFG/transitive-closure-of-a-graph.cpp
+++ Transitive closure of a Graph - GFG/transitive-closure-of-a-graph.cpp
@@ -15,8 +15,7 @@ public:
         for(int k=0;k<n;k++) {
             for(int i=0;i<n;i++) {
                 for(int j=0;j<n;j++) {
-                    if(i==j) grid[i][j] = 1;
-                    else if(grid[i][j]==0 && grid[i][k]==1 && grid[k][j]==1) grid[i][j]=1;
+                    if(i==j || grid[i][j]==0 && grid[i][k]==1 && grid[k][j]==1) grid[i][j] = 1;
                 }
             }
         }
