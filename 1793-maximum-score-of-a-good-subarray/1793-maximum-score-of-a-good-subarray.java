346b9636a832b46e855d5689ceaccc4bc637b8b6
Neelotpal Biswas <neelotpal10@gmail.com>
2023-10-22 17:47:11 +0530

    Time: 7 ms (80.56%), Space: 59.2 MB (32.41%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.java
@@ -1,26 +1,15 @@
 class Solution {
     public int maximumScore(int[] nums, int k) {
         int n = nums.length;
-        int left[] = new int[n];
-        Arrays.fill(left, -1);
-        Stack<Integer> stack = new Stack<>();
+        int left = k;
+        int right = k;
+        int ans = nums[k];
+        int currMin = nums[k];
         
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) left[stack.pop()] = i;
-            stack.push(i);
-        }
-        
-        int right[] = new int[n];
-        Arrays.fill(right, n);
-        stack = new Stack<>();
-        
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) right[stack.pop()] = i;
-            stack.push(i);
-        }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            if (left[i] < k && right[i] > k) ans = Math.max(ans, nums[i] * (right[i] - left[i] - 1));
+        while (left > 0 || right < n - 1) {
+            if ((left > 0 ? nums[left - 1]: 0) < (right < n - 1 ? nums[right + 1] : 0)) currMin = Math.min(currMin, nums[++right]);
+            else currMin = Math.min(currMin, nums[--left]);
+            ans = Math.max(ans, currMin * (right - left + 1));
         }
         return ans;
     }
