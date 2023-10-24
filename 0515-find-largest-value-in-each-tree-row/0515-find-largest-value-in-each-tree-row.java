59ba7db896b786027c21e2c65971086cb6cf4e8f
Neelotpal Biswas <neelotpal10@gmail.com>
2023-10-24 11:11:48 +0530

    Time: 1 ms (98.83%), Space: 44.1 MB (61.01%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -14,22 +14,26 @@
  * }
  */
 class Solution {
+    List<Integer> ans;
+    
     public List<Integer> largestValues(TreeNode root) {
-        if (root == null) return new ArrayList<Integer>();
-        List<Integer> ans = new ArrayList<Integer>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int currentLength = queue.size();
-            int currMax = Integer.MIN_VALUE;
-            for (int i = 0; i < currentLength; i++) {
-                TreeNode node = queue.remove();
-                currMax = Math.max(currMax, node.val);
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-            }
-            ans.add(currMax);
-        }
+        ans = new ArrayList<Integer>();
+        dfs(root, 0);
         return ans;
+    }
+    
+    public void dfs(TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+        
+        if (depth == ans.size()) {
+            ans.add(node.val);
+        } else {
+            ans.set(depth, Math.max(ans.get(depth), node.val));
+        }
+        
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
     }
 }
