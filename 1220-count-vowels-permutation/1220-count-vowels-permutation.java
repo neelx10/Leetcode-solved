e2c197aba1d1af75f79f4fd7239f273e26de9d67
Neelotpal Biswas <neelotpal10@gmail.com>
2023-10-28 14:26:27 +0530

    Time: 15 ms (58.09%), Space: 43.5 MB (46.53%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,28 +1,30 @@
 class Solution {
     public int countVowelPermutation(int n) {
         long MOD = (long)(1e9 + 7);
-        long[][] dp = new long[n + 1][6];
-        Arrays.fill(dp[0], 1);
+        long[] dpLast = new long[6];
+        Arrays.fill(dpLast, 1);
         for (int len = 1; len <= n; len++) {
+            long[] dpCurr = new long[6];
             for (int last = 0; last <= 5; last++) {
                 long count = 0;
                 if (last == 2)
-                    count = (count + dp[len - 1][1]) % MOD;
+                    count = (count + dpLast[1]) % MOD;
                 if (last == 1 || last == 3)
-                    count = (count + dp[len - 1][2]) % MOD;
+                    count = (count + dpLast[2]) % MOD;
                 if (last == 1 || last == 2 || last == 4 || last == 5)
-                    count = (count + dp[len - 1][3]) % MOD;
+                    count = (count + dpLast[3]) % MOD;
                 if (last == 3 || last == 5)
-                    count = (count + dp[len - 1][4]) % MOD;
+                    count = (count + dpLast[4]) % MOD;
                 if (last == 1)
-                    count = (count + dp[len - 1][5]) % MOD;
+                    count = (count + dpLast[5]) % MOD;
                 if (last == 0)
                     for (int i = 1; i <= 5; i++)
-                        count = (count + dp[len - 1][i]) % MOD;
+                        count = (count + dpLast[i]) % MOD;
 
-                dp[len][last] = count % MOD;
+                dpCurr[last] = count;
             }
+            dpLast = dpCurr;
         }
-        return (int)dp[n][0];
+        return (int)dpLast[0];
     }
 }
