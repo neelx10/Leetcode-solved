ce106b68f382912f071c52a52f4fe24f07a79839
Neelotpal Biswas <neelotpal10@gmail.com>
2023-10-28 14:25:52 +0530

    Time: 22 ms (32.01%), Space: 43.2 MB (65.35%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,34 +1,28 @@
 class Solution {
-    long MOD = (long)(1e9 + 7);
-    
     public int countVowelPermutation(int n) {
-        long[][] memo = new long[n + 1][6];
-        for (long[] row : memo) Arrays.fill(row, -1);
-        return (int)findPerm(n, 0, memo);
-    }
-    
-    private long findPerm(int len, int last, long[][] memo) {
-        if (len == 0)
-            return 1;
+        long MOD = (long)(1e9 + 7);
+        long[][] dp = new long[n + 1][6];
+        Arrays.fill(dp[0], 1);
+        for (int len = 1; len <= n; len++) {
+            for (int last = 0; last <= 5; last++) {
+                long count = 0;
+                if (last == 2)
+                    count = (count + dp[len - 1][1]) % MOD;
+                if (last == 1 || last == 3)
+                    count = (count + dp[len - 1][2]) % MOD;
+                if (last == 1 || last == 2 || last == 4 || last == 5)
+                    count = (count + dp[len - 1][3]) % MOD;
+                if (last == 3 || last == 5)
+                    count = (count + dp[len - 1][4]) % MOD;
+                if (last == 1)
+                    count = (count + dp[len - 1][5]) % MOD;
+                if (last == 0)
+                    for (int i = 1; i <= 5; i++)
+                        count = (count + dp[len - 1][i]) % MOD;
 
-        if (memo[len][last] != -1)
-            return memo[len][last];
-
-        long count = 0;
-        if (last == 2)
-            count = (count + findPerm(len - 1, 1, memo)) % MOD;
-        if (last == 1 || last == 3)
-            count = (count + findPerm(len - 1, 2, memo)) % MOD;
-        if (last != 0 && last != 3)
-            count = (count + findPerm(len - 1, 3, memo)) % MOD;
-        if (last == 3 || last == 5)
-            count = (count + findPerm(len - 1, 4, memo)) % MOD;
-        if (last == 1)
-            count = (count + findPerm(len - 1, 5, memo)) % MOD;
-        if (last == 0)
-            for (int i = 1; i <= 5; i++)
-                count = (count + findPerm(len - 1, i, memo)) % MOD;
-
-        return memo[len][last] = count % MOD;
+                dp[len][last] = count % MOD;
+            }
+        }
+        return (int)dp[n][0];
     }
 }
