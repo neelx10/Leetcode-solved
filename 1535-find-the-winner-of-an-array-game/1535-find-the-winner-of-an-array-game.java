9143a0eeff525c3554930f539c9789216ee88f1b
Neelotpal Biswas <neelotpal10@gmail.com>
2023-11-05 11:33:21 +0530

    Time: 1 ms (52.90%), Space: 56.2 MB (81.16%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.java
@@ -1,19 +1,14 @@
 class Solution {
     public int getWinner(int[] arr, int k) {
-        int maxElement = arr[0];
-        for (int i = 1; i < arr.length; i++) maxElement = Math.max(maxElement, arr[i]);
-        
-        int curr = arr[0];
-        int winstreak = 0;
-        for (int i = 1; i < arr.length; i++) {
-            int opponent = arr[i];
-            if (curr > opponent) winstreak++;
-            else{
-                curr = opponent;
-                winstreak = 1;
+        int win=0;
+        int ele=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>ele){
+                win=0;
+                ele=arr[i];
             }
-            if (winstreak == k || curr == maxElement) return curr;
+            if(++win==k) break;
         }
-        return -1;
+        return ele;
     }
 }
