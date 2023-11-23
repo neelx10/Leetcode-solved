6abdf367ede8ff10b14ba5a4be0c9786bc6397a7
Neelotpal Biswas <neelotpal10@gmail.com>
2023-11-23 18:31:01 +0530

    Time: 30 ms (32.14%), Space: 45 MB (9.62%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,9 +1,22 @@
 class Solution {
     public Boolean check(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[i] - arr[i - 1] != diff) return false;
+        int minElement = Integer.MAX_VALUE;
+        int maxElement = Integer.MIN_VALUE;
+        Set<Integer> arrSet = new HashSet();
+        
+        for (int num : arr) {
+            minElement = Math.min(minElement, num);
+            maxElement = Math.max(maxElement, num);
+            arrSet.add(num);
+        }
+        
+        if ((maxElement - minElement) % (arr.length - 1) != 0) return false;
+        int diff = (maxElement - minElement) / (arr.length - 1);
+        int curr = minElement + diff;
+        
+        while (curr < maxElement) {
+            if (!arrSet.contains(curr)) return false;
+            curr += diff;
         }
         return true;
     }
