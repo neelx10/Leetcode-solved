f8e1e0f843e18be934816cba32051e1afc5bcc0c
Neelotpal Biswas <neelotpal10@gmail.com>
2023-11-27 12:54:56 +0530

    Time: 12 ms (95.38%), Space: 43.5 MB (52.99%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -12,19 +12,22 @@ class Solution {
             {1, 3},
             {2, 4}
         };
-        
+
         int MOD = (int) 1e9 + 7;
-        int[][] dp = new int[n][10];
-        for (int square = 0; square < 10; square++) dp[0][square] = 1;
+        int[] dp = new int[10];
+        int[] prevDp = new int[10];
+        Arrays.fill(prevDp, 1);
         for (int remain = 1; remain < n; remain++) {
+            dp = new int[10];
             for (int square = 0; square < 10; square++) {
                 int ans = 0;
-                for (int nextSquare : jumps[square]) ans = (ans + dp[remain - 1][nextSquare]) % MOD;
-                dp[remain][square] = ans;
+                for (int nextSquare : jumps[square]) ans = (ans + prevDp[nextSquare]) % MOD;
+                dp[square] = ans;
             }
+            prevDp = dp;
         }
         int ans = 0;
-        for (int square = 0; square < 10; square++) ans = (ans + dp[n - 1][square]) % MOD;
+        for (int square = 0; square < 10; square++) ans = (ans + prevDp[square]) % MOD;
         return ans;
     }
 }
