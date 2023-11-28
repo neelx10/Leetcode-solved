44a8abb3888bcaf927e4a7fafd5ea3c6f88feebd
Neelotpal Biswas <neelotpal10@gmail.com>
2023-11-28 23:30:27 +0530

    Time: 167 ms (39.47%), Space: 55.1 MB (34.21%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -1,18 +1,22 @@
 class Solution {
     public int numberOfWays(String corridor) {
         final int MOD = 1_000_000_007;
-        int zero = 0;
-        int one = 0;
-        int two = 1;
-        for (char thing : corridor.toCharArray()) {
-            if (thing == 'S') {
-                zero = one;
-                int temp = one;
-                one = two;
-                two = temp;
+        int[][] count = new int[corridor.length() + 1][3];
+        count[corridor.length()][0] = 0;
+        count[corridor.length()][1] = 0;
+        count[corridor.length()][2] = 1;
+        for (int index = corridor.length() - 1; index >= 0; index--) {
+            if (corridor.charAt(index) == 'S') {
+                count[index][0] = count[index + 1][1];
+                count[index][1] = count[index + 1][2];
+                count[index][2] = count[index + 1][1];
             } 
-            else two = (two + zero) % MOD;
+            else{
+                count[index][0] = count[index + 1][0];
+                count[index][1] = count[index + 1][1];
+                count[index][2] = (count[index + 1][0] + count[index + 1][2]) % MOD;
+            }
         }
-        return zero;
+        return count[0][0];
     }
 }
