88746f6aa41f937a971d909c996a059da43ca6ac
Neelotpal Biswas <neelotpal10@gmail.com>
2023-11-28 23:28:55 +0530

    Time: 21 ms (97.37%), Space: 44.7 MB (68.42%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.java
@@ -1,22 +1,18 @@
 class Solution {
-    private int MOD = (int)(1e9+7);
-    private int count(int index, int seats, String corridor, Map<Pair<Integer, Integer>, Integer> cache) {
-        if (index == corridor.length()) return seats == 2 ? 1 : 0;
-        if (cache.containsKey(new Pair<>(index, seats))) return cache.get(new Pair<>(index, seats));
-        int result = 0;
-        if (seats == 2) {
-            if (corridor.charAt(index) == 'S') result = count(index + 1, 1, corridor, cache);
-            else result = (count(index + 1, 0, corridor, cache) + count(index + 1, 2, corridor, cache)) % MOD;  
-        }
-        else{
-            if (corridor.charAt(index) == 'S') result = count(index + 1, seats + 1, corridor, cache);
-            else result = count(index + 1, seats, corridor, cache);
-        }
-        cache.put(new Pair<>(index, seats), result);
-        return result;
-    }
     public int numberOfWays(String corridor) {
-        Map<Pair<Integer, Integer>, Integer> cache = new HashMap<>();
-        return count(0, 0, corridor, cache);
+        final int MOD = 1_000_000_007;
+        int zero = 0;
+        int one = 0;
+        int two = 1;
+        for (char thing : corridor.toCharArray()) {
+            if (thing == 'S') {
+                zero = one;
+                int temp = one;
+                one = two;
+                two = temp;
+            } 
+            else two = (two + zero) % MOD;
+        }
+        return zero;
     }
 }
