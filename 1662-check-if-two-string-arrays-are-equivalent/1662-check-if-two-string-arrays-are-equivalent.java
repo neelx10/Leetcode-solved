2ffa073d4a6f4ba98ade1497a7ac7564457c6161
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-01 18:55:43 +0530

    Time: 1 ms (73.30%), Space: 41.1 MB (7.59%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,21 +1,9 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        int word1Pointer = 0, word2Pointer = 0;
-        int string1Pointer = 0, string2Pointer = 0;
-        while (word1Pointer < word1.length && word2Pointer < word2.length) {
-            if (word1[word1Pointer].charAt(string1Pointer++) != 
-                word2[word2Pointer].charAt(string2Pointer++)) {
-                return false;
-            }
-            if (string1Pointer == word1[word1Pointer].length()) {
-                word1Pointer++;
-                string1Pointer = 0;
-            }
-            if (string2Pointer == word2[word2Pointer].length()) {
-                word2Pointer++;
-                string2Pointer = 0;
-            }
-        }
-        return word1Pointer == word1.length && word2Pointer == word2.length;
+        StringBuilder word1Combined = new StringBuilder();
+        for (String s : word1) word1Combined.append(s);
+        StringBuilder word2Combined = new StringBuilder();
+        for (String s : word2) word2Combined.append(s);
+        return word1Combined.compareTo(word2Combined) == 0;
     }
 }
