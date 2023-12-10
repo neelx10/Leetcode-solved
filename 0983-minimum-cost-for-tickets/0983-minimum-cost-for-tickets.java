3e9fcd5da290b319159ac643b7cbe554d12e6111
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-11 03:38:03 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (26.32%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,20 +1,23 @@
 class Solution {
+    public int solve(int[] days,int[] costs, int n,int idx,int[] dp){
+        if(idx==n) return 0;
+        if(dp[idx]!=0) return dp[idx];
+            
+        int cost1=costs[0]+solve(days,costs,n,idx+1,dp);
+        
+        int i=idx;
+        while(i<n && days[i]<days[idx]+7) i++;
+        int cost2=costs[1]+solve(days,costs,n,i,dp);
+        
+        i=idx;
+        while(i<n && days[i]<days[idx]+30) i++;
+        int cost3=costs[2]+solve(days,costs,n,i,dp);
+        
+        return dp[idx]=Math.min(cost1,Math.min(cost2,cost3));
+    }
     public int mincostTickets(int[] days, int[] costs) {
         int n=days.length;
-        int[] dp=new int[n+1];
-        
-        for(int idx=n-1;idx>=0;idx--){
-            int cost1=costs[0]+dp[idx+1];
-        
-            int i;
-            for(i=idx;i<n && days[i]<days[idx]+7;i++);
-            int cost2=costs[1]+dp[i];
-
-            for(i=idx;i<n && days[i]<days[idx]+30;i++);
-            int cost3=costs[2]+dp[i];
-
-            dp[idx]=Math.min(cost1,Math.min(cost2,cost3));
-        }
-        return dp[0];
+        int[] dp=new int[n];
+        return solve(days,costs,n,0,dp);
     }
 }
