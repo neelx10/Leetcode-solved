4b43271a75ff1ef5a869cbf2c9668190e9b96774
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-11 22:06:42 +0530

    Time: 1 ms (76.21%), Space: 44 MB (85.65%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int trap(int[] arr) {
+        int n=arr.length;
+        int[] prefix=new int[n];
+        int[] suffix=new int[n];
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++) prefix[i]=Math.max(prefix[i-1],arr[i]);
+        suffix[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--) suffix[i]=Math.max(suffix[i+1],arr[i]);
+        int sum=0;
+        for(int i=0;i<n;i++) sum+=Math.min(prefix[i],suffix[i])-arr[i];
+        return sum;
+    }
+}
