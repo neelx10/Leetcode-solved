70a084f6939f3270a96e8f317d6767babd0d67da
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-11 15:28:09 +0530

    Time: 294 ms (25.48%), Space: 44.3 MB (83.01%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,17 +1,19 @@
 class Solution {
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        int[][] dp=new int[piles.size()+1][k+1];
+        int[] next=new int[k+1];
+        int[] curr=new int[k+1];
         for(int i=piles.size()-1;i>=0;i--){
             for(int K=0;K<=k;K++){
-                int notTake=dp[i+1][K];
+                int notTake=next[K];
                 int sum=0,take=(int)-1e9;
                 for(int j=0;j<Math.min(K,piles.get(i).size());j++){
                     sum+=piles.get(i).get(j);
-                    take=Math.max(take,sum+dp[i+1][K-(j+1)]);
+                    take=Math.max(take,sum+next[K-(j+1)]);
                 }
-                dp[i][K]=Math.max(take,notTake);
+                curr[K]=Math.max(take,notTake);
             }
+            next=curr.clone();
         }
-        return dp[0][k];
+        return next[k];
     }
 }
