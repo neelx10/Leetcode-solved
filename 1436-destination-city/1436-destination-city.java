c8f446af89a56e4fda597b865a44af804a737bf5
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-15 13:26:54 +0530

    Time: 2 ms (79.52%), Space: 43.3 MB (23.46%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Map<String,String> map=new HashMap<>();
+        for(List<String> path:paths) map.put(path.get(0),path.get(1));
+        for(String city:map.values()){
+            if(!map.containsKey(city)) return city;
+        }
+        return "";
+    }
+}
