db2d0824015f3689387800c332b7c339f1c73278
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-15 13:30:41 +0530

    Time: 2 ms (79.52%), Space: 43.3 MB (31.72%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -1,9 +1,9 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        Map<String,String> map=new HashMap<>();
-        for(List<String> path:paths) map.put(path.get(0),path.get(1));
-        for(String city:map.values()){
-            if(!map.containsKey(city)) return city;
+        Set<String> hasOutgoing = new HashSet();
+        for (int i = 0; i < paths.size(); i++) hasOutgoing.add(paths.get(i).get(0));
+        for (int i = 0; i < paths.size(); i++) {
+            if (!hasOutgoing.contains(paths.get(i).get(1))) return paths.get(i).get(1);
         }
         return "";
     }
