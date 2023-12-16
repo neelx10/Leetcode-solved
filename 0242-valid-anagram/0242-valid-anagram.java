f7f32bdaf578dd36a29e6b8b984fa29e13aefc54
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-16 12:08:31 +0530

    Time: 5 ms (54.58%), Space: 41.7 MB (95.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int[] sarr=new int[26];
+        for(int i=0;i<s.length();i++){
+            sarr[s.charAt(i)-'a']++;
+            sarr[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(sarr[i]<0 || sarr[i]>0) return false;
+        }
+        return true;
+    }
+}
