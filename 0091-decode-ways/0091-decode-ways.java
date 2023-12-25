d6bb930a63a35431b8d6e29c6e75fda575456beb
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-25 13:20:40 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (11.16%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int solve(int i,String s,int[] dp){
-        if(i==s.length()) return 1;
-        if(s.charAt(i)=='0') return 0;
-        if(dp[i]!=0) return dp[i];
-        int ways=solve(i+1,s,dp);
-        if(i<s.length()-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) <= '6'))) ways+=solve(i+2,s,dp);
-        return dp[i]=ways;
-    }
     public int numDecodings(String s) {
         int n=s.length();
         int[] dp=new int[n+1];
-        return solve(0,s,dp);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(!(s.charAt(i)=='0')){
+                int ways=dp[i+1];
+                if(i<s.length()-1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) <= '6'))) ways+=dp[i+2];
+                dp[i]=ways;
+            }
+        }
+        return dp[0];
     }
 }
