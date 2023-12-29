bdcf324507118e36bb1e75796a2cf210abd1044e
Neelotpal Biswas <neelotpal10@gmail.com>
2023-12-29 13:16:39 +0530

    Time: 14 ms (27.53%), Space: 41.5 MB (13.36%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -3,10 +3,11 @@ class Solution {
         int n=arr.length;
         if(n<d) return -1;
         int[][] dp=new int[n+1][d+1];
-        for(int ind=0;ind<n;ind++){
-            int maxi=arr[ind];
-            for(int i=ind;i<n;i++) maxi=Math.max(maxi,arr[i]);
-            dp[ind][1]=maxi;
+        int mx=arr[n-1];
+        dp[n-1][1]=mx;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>mx) mx=arr[i];
+            dp[i][1]=mx;
         }
         for(int ind=n-1;ind>=0;ind--){
             for(int days=2;days<=d;days++){
