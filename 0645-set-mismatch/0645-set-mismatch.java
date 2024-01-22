36196f6c72dfba8724f3827956263a9a7af26c47
Neelotpal Biswas <neelotpal10@gmail.com>
2024-01-22 13:52:25 +0530

    Time: 15 ms (11.03%), Space: 45.2 MB (69.63%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2];
+        Arrays.sort(nums);
+        // Find the duplicate number
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                result[0] = nums[i];
+                break;
+            }
+        }
+        // Find the missing number
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = Arrays.stream(nums).sum();
+        result[1] = expectedSum - (actualSum - result[0]);
+        return result;
+    }
+}
