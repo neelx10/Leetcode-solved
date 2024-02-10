d73e862e359f783dc4314dd9cda5e509503e106b
Neelotpal Biswas <neelotpal10@gmail.com>
2024-02-10 20:20:12 +0530

    Time: 12 ms (31.89%), Space: 45.2 MB (19.44%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -4,21 +4,10 @@ class Solution {
         boolean[][] palindrome = new boolean[n][n];
         int ans = 0;
 
-        for(int i=0;i<n;i++) {
-            palindrome[i][i] = true;
-            ans++;
-        }
-
-        for(int i=0;i<n-1;i++) {
-            if(s.charAt(i) == s.charAt(i+1)) {
-                palindrome[i][i+1] = true;
-                ans++;
-            }
-        }
-
-        for(int len=3;len<=n;len++) {
+        for(int len=1;len<=n;len++) {
             for(int i=0;i<n-len+1;i++) {
-                if(s.charAt(i) == s.charAt(i+len-1) && palindrome[i+1][i+len-2]) {
+                if(s.charAt(i) == s.charAt(i+len-1) 
+                    && (len <= 2 || palindrome[i+1][i+len-2])) {
                     palindrome[i][i+len-1] = true;
                     ans++;
                 }
