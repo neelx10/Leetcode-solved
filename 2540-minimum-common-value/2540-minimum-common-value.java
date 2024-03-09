5e022a6267362af4c8ee986725c906aa6c120ad8
Neelotpal Biswas <neelotpal10@gmail.com>
2024-03-09 11:43:52 +0530

    Time: 1 ms (96.67%), Space: 62.5 MB (57.41%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,18 +1,10 @@
 public class Solution {
-    private boolean binarySearch(int target, int[] nums) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target) right = mid - 1;
-            else if (nums[mid] < target) left = mid + 1;
-            else return true;
-        }
-        return false;
-    }
     public int getCommon(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) return getCommon(nums2, nums1);
-        for (int num : nums1) {
-            if (binarySearch(num, nums2)) return num;
+        int first = 0,second = 0;
+        while (first < nums1.length && second < nums2.length) {
+            if (nums1[first] < nums2[second]) first++;
+            else if (nums1[first] > nums2[second]) second++;
+            else return nums1[first];
         }
         return -1;
     }
