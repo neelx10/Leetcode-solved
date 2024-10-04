3a4765bba56313532c2e7b221590215a74473b43
Neelotpal Biswas <neelotpal10@gmail.com>
2024-10-04 10:10:20 +0530

    Time: 2 ms (100.00%), Space: 55 MB (99.24%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long dividePlayers(int[] skill) {
+        int n = skill.length, totalSkill = 0;
+        int[] skillFrequency = new int[1001];
+        for (int playerSkill : skill) {
+            totalSkill += playerSkill;
+            skillFrequency[playerSkill]++;
+        }
+        if (totalSkill % (n / 2) != 0) return -1;
+        int targetTeamSkill = totalSkill / (n / 2);
+        long totalChemistry = 0;
+        for (int playerSkill : skill) {
+            int partnerSkill = targetTeamSkill - playerSkill;
+            if (skillFrequency[partnerSkill] == 0)  return -1;
+            totalChemistry += (long) playerSkill * (long) partnerSkill;
+            skillFrequency[partnerSkill]--;
+        }
+        return totalChemistry / 2;
+    }
+}
